5f47bbf6c73226bb16cb66f97f430dc942eaa2bc
Fabio Silva <fsilva@usc.edu>
2025-12-01 11:59:54 -0800

    Added new fas nga codes.

---
+++ gmsvtoolkit/metrics/fas.py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python3
+"""
+BSD 3-Clause License
+
+Copyright (c) 2025, University of Southern California
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This module is used to calculate FAS
+"""
+from __future__ import division, print_function
+
+# Import Python modules
+import os
+import sys
+import glob
+import time
+import atexit
+import shutil
+import numpy as np
+import argparse
+import tempfile
+
+# Imports needed from the GMSVToolkit
+from core import exceptions
+from utils import os_utilities
+from utils import file_utilities
+from core.station_list import StationList
+
+# Import FAS functions
+from metrics import fas_eas
+
+B_PARAM = 188.5
+W_PARAM = 1.0/(10.0**(3.0/B_PARAM))
+CM2G = 980.664999
+
+def cleanup(dir_name):
+    """
+    This function removes the temporary directory
+    """
+    shutil.rmtree(dir_name)
+
+def find_acc_file(input_dir, station_name, label):
+    """
+    Looks into input_dir for a acceleration seismogram for station station_name
+    """
+    # Find input file
+    input_list = glob.glob("%s%s*%s.%s*.bbp" %
+                           (input_dir, os.sep, label, station_name))
+    if not len(input_list):
+        # Try to match filename without the label
+        input_list = glob.glob("%s%s%s.bbp" %
+                               (input_dir, os.sep, station_name))
+        if not len(input_list):
+            print("[ERROR]: Can't find input file for station %s" % (station_name))
+            sys.exit(1)
+    if len(input_list) > 1:
+        # Found more than one file, check if we can find a single file that includes .acc.bbp
+        input_list = [filename for filename in input_list if ".acc.bbp" in filename]
+        if len(input_list) > 1:
+            print("[ERROR]: Found multiple input files for station %s" % (station_name))
+            sys.exit(1)
+
+    input_file = os.path.basename(input_list[0])
+
+    return input_file
+
+def compute_fas(acc1, acc2, dt):
+    """
+    Computes FAS/EAS/SEAS for the provided horizontal components
+    """
+    b = B_PARAM
+    w = W_PARAM
+
+    # Compute fas, eas, and smoothed eas (seas)
+    [fas_freq, fas_1, fas_2, eas,
+     seas_freq, seas] = fas_eas.get_smooth_eas(acc1, acc2, dt, b, w)
+
+    return fas_freq, fas_1, fas_2, eas, seas_freq, seas
+
+def compute_station_fas(a_tmpdir, a_outdir_fas, acc_file,
+                        station_name, output_prefix,
+                        input_unit="cm/s/s", output_unit="cm/s/s",
+                        logfile=None):
+    """
+    Computes FAS for a station
+    """
+    # Read dt from input file
+    dt = file_utilities.read_bbp_dt(acc_file)
+    # Read two horizontal components from input file
+    [_, acc1, acc2, _] = file_utilities.read_bbp_file(acc_file)
+
+    # Convert units if needed
+    if input_unit == "cm/s/s" and output_unit == "g":
+        # Convert units from cm/s/s to g
+        acc1 = acc1 / CM2G
+        acc2 = acc2 / CM2G
+    if input_unit == "g" and output_unit == "cm/s/s":
+        acc1 = acc1 * CM2G
+        acc2 = acc2 * CM2G
+
+    [fas_freq, fas_1, fas_2, eas,
+     seas_freq, seas] = compute_fas(acc1, acc2, dt)
+    
+    # Write fas/eas file first
+    output_basename = "%s.eas.fs.col" % (output_prefix)
+    output_filename = os.path.join(a_outdir_fas, output_basename)
+    output_file = open(output_filename, 'w')
+    output_file.write("# Freq(Hz)      FAS H1 (%s)      FAS H2 (%s)      EAS (%s)\n" %
+                      (output_unit, output_unit, output_unit))
+    for f0, f1, f2, e0 in zip(fas_freq, fas_1, fas_2, eas):
+        output_file.write("%2.7E\t%2.7E\t%2.7E\t%2.7E\n" %
+                          (f0, f1, f2, e0))
+    output_file.close()
+
+    # write seas next
+    output_basename = "%s.seas.fs.col" % (output_prefix)
+    output_filename = os.path.join(a_outdir_fas, output_basename)
+    output_file = open(output_filename, 'w')
+    output_file.write("# Freq(Hz)      Smoothed EAS (%s) b=%3.4f\n" %
+                      (output_unit, B_PARAM))
+    for f0, s0 in zip(seas_freq, seas):
+        output_file.write("%2.7E\t%2.7E\n" %
+                          (f0, s0))
+    output_file.close()
+
+class FAS(object):
+    """
+    Implement FAS analisys for the Broadband Platform
+    """
+    def __init__(self):
+        """
+        Initializes class variables
+        """
+        pass
+
+    def parse_arguments(self):
+        """
+        This function takes care of parsing the command-line arguments and
+        asking the user for any missing parameters that we need
+        """
+        parser = argparse.ArgumentParser(description="Compute FAS "
+                                         " for a set of seismograms.")
+        parser.add_argument("--station-list", "-s", dest="station_list", required=True,
+                            help="station list for batch processing")
+        parser.add_argument("--output-dir", dest="output_dir",
+                            help="output directory")
+        parser.add_argument("--logfile", dest="logfile",
+                            help="file to store processing log messages")
+        parser.add_argument("--labels", "-l", dest="labels", required=True,
+                            help="comma-separated comparison labels")
+        parser.add_argument("--input-unit", dest="input_unit", default="cm/s/s",
+                            help="input units: (g or cm/s/s)")
+        parser.add_argument("--output-unit", dest="output_unit", default="cm/s/s",
+                            help="output units: (g or cm/s/s)")
+        parser.add_argument('input_folders', nargs='*')
+        
+        args = parser.parse_args()
+        return args
+
+    def run(self):
+        """
+        Run FAS analysis codes
+        """
+        # Parse command-line options
+        args = self.parse_arguments()
+
+        output_dir = ""
+        logfile = None
+        if args.output_dir is not None:
+            output_dir = args.output_dir
+        output_dir = os.path.abspath(output_dir)
+        if args.logfile is not None:
+            logfile = os.path.abspath(args.logfile)
+        station_file = os.path.abspath(args.station_list)
+
+        input_unit = args.input_unit.lower()
+        output_unit = args.output_unit.lower()
+        if input_unit != "g" and input_unit != "cm/s/s":
+            print("[ERROR]: Input unit must be 'g' or 'cm/s/s'")
+            sys.exit(-1)
+        if output_unit != "g" and output_unit != "cm/s/s":
+            print("[ERROR]: Output unit must be 'g' or 'cm/s/s'")
+            sys.exit(-1)
+        
+        # Sort input folders/labels
+        input_folders = args.input_folders
+        labels = args.labels
+        if len(input_folders) < 1:
+            print("[ERROR]: Please specify at least one input folder!")
+            sys.exit(1)
+        labels = [label.strip() for label in labels.split(",")]
+        if len(labels) != len(input_folders):
+            print("[ERROR] Please specify as many labels as input folders!")
+            sys,exit(1)
+
+        self.run_fas_eas(station_file, input_folders, labels,
+                         output_dir, input_unit=input_unit,
+                         output_unit=output_unit,
+                         logfile=logfile, temp_dir=None)
+            
+    def run_fas_eas(self, station_file, input_folders, labels,
+                    output_dir, input_unit, output_unit,
+                    logfile=None, temp_dir=None):
+        """
+        Run FAS analysis codes
+        """
+        sta_base = os.path.basename(os.path.splitext(station_file)[0])
+
+        if temp_dir is None:
+            # Create temp directory if needed
+            temp_dir = tempfile.mkdtemp()
+            # And clean up later
+            atexit.register(cleanup, temp_dir)
+
+        # Make paths absolute paths
+        temp_dir = os.path.abspath(temp_dir)
+        output_dir = os.path.abspath(output_dir)
+        input_folders = [os.path.abspath(input_folder) for input_folder in input_folders]
+
+        if logfile is None:
+            # Create our own log file
+            logfile = os.path.join(temp_dir,
+                                   "%s.fas.log" % ("-".join(labels)))
+        #
+        # Make sure the tmp and out directories exist
+        #
+        os_utilities.mkdirs([temp_dir, output_dir], print_cmd=False)
+
+        slo = StationList(station_file)
+        site_list = slo.get_station_list()
+
+        for input_folder, label in zip(input_folders, labels):
+            # Process each input folder
+            print("==> Processing %s" % (label))
+            for station in site_list:
+                # Process each station
+                station_name = station.scode
+                print("===> Processing station: %s..." % (station_name),
+                      end="", flush=True)
+                acc_file = find_acc_file(input_folder, station_name, label)
+                input_acc_file = os.path.join(input_folder, acc_file)
+                output_prefix = "%s.%s" % (label, station_name)
+                
+                # Compute seismogram's FAS
+                t1 = time.time()
+                compute_station_fas(temp_dir, output_dir, input_acc_file,
+                                    station_name, output_prefix=output_prefix,
+                                    input_unit=input_unit, output_unit=output_unit,
+                                    logfile=logfile)
+                t2 = time.time()
+                print("%10.2f s" % (t2 - t1))
+                
+        # All done
+        print("=> Processing done!")
+
+if __name__ == '__main__':
+    ME = FAS()
+    ME.run()
+    sys.exit(0)
---
+++ gmsvtoolkit/metrics/fas_eas.py
@@ -0,0 +1,167 @@
+"""
+Copyright 2023 Scott J. Brandenberg
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the “Software”),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import numpy as np
+
+def smoothed_fas(fas, f, fc, b, w):
+    '''
+    Input Fourier amplitude spectrum, frequency array, smoothed frequency
+    array, and Konno-Omachi smoothing parameters b and w. Export smoothed
+    Fourier amplitude spectrum.
+
+    fas = Fourier amplitude spectrum. Real-valued numpy array.
+    f = Frequency array. Real-valued numpy array.
+    fc = Frequency values for smoothed Fourier spectrum.
+         Real-valued numpy array.
+    b = Konno-Omachi smoothing parameter. Real-valued scalar.
+    w = Konno-Omachi bandwidth parameter. Real-valued scalar.
+    fac_c = Smoothed Fourier amplitude spectrum. Real-valued numpy array.
+    '''
+    fdivfc = f[:, np.newaxis]/fc
+    filt = (fdivfc > w) & (fdivfc < 1/w) & (abs(fdivfc-1.0) > 1.e-6)
+    weights = np.zeros(fdivfc.shape)
+    weights[abs(fdivfc-1.0) < 1.e-6] = 1.0
+    weights[filt] = abs(np.sin(b*np.log10(fdivfc[filt]))/(b*np.log10(fdivfc[filt])))**4.0
+    weights = weights/np.sum(weights, axis=0)
+    fas_c = fas@weights
+    return fas_c
+
+def get_smooth_eas(acc1, acc2, dt, b, w):
+    '''
+    Input two acceleration time series, time step, and Konno-Omachi
+    smoothing parameters.
+    
+    Return smooth effective amplitude spectrum.
+    acc1, acc2 = acceleration time series. Real-valued numpy arrays.
+    dt = time step in seconds. Real-valued.
+    b = Konno-Omachi smoothing parameter. Real-valued scalar.
+    w = Konno-Omachi bandwidth parameter. Real-valued scalar.
+    fc = Frequency values for smoothed spectrum. Real-valued numpy array.
+    eas = effective amplitude spectrum. Real-valued numpy array.
+    '''
+    dt_vec = [0.005, 0.01, 0.02, 0.025, 0.05, 0.10]
+    nfft_vec = [19, 18, 17, 17, 16, 15]
+    nfft = 2**int(np.interp(dt, dt_vec, nfft_vec))
+    
+    f1 = np.fft.rfft(acc1, n=nfft) * dt
+    f2 = np.fft.rfft(acc2, n=nfft) * dt
+    eas = 0.5 * (np.abs(f1)**2 + np.abs(f2)**2)
+    freq = np.fft.rfftfreq(nfft, d=dt)
+    fc = np.asarray([0.01318257, 0.01348963, 0.01380385, 0.01412538,
+                     0.0144544, 0.01479109, 0.01513562, 0.01548817,
+                     0.015848932, 0.016218101, 0.01659587, 0.01698244,
+                     0.01737801, 0.017782792, 0.01819701, 0.01862087,
+                     0.019054604, 0.01949845, 0.019952621, 0.02041738,
+                     0.02089296, 0.021379621, 0.02187762, 0.022387214,
+                     0.02290868, 0.02344229, 0.02398833, 0.02454709,
+                     0.02511887, 0.025703962, 0.026302684,
+                     0.026915352, 0.027542291, 0.028183832,
+                     0.02884032, 0.029512094, 0.03019952, 0.03090296,
+                     0.031622774, 0.032359361, 0.03311311,
+                     0.033884413, 0.03467368, 0.035481333, 0.0363078,
+                     0.03715353, 0.038018941, 0.03890452, 0.03981072,
+                     0.040738031, 0.04168694, 0.04265795, 0.04365158,
+                     0.04466836, 0.04570882, 0.046773523, 0.04786302,
+                     0.04897789, 0.05011873, 0.051286142, 0.05248075,
+                     0.053703181, 0.05495409, 0.056234132,
+                     0.057543992, 0.058884363, 0.06025596, 0.0616595,
+                     0.06309573, 0.064565412, 0.06606936, 0.067608304,
+                     0.0691831, 0.070794582, 0.0724436, 0.07413102,
+                     0.075857751, 0.07762471, 0.079432822, 0.08128305,
+                     0.08317637, 0.085113793, 0.08709637, 0.089125104,
+                     0.0912011, 0.09332544, 0.095499262, 0.097723722,
+                     0.1, 0.1023293, 0.1047129, 0.10715192,
+                     0.10964782, 0.11220184, 0.1148154, 0.1174898,
+                     0.12022643, 0.1230269, 0.12589254, 0.12882494,
+                     0.1318257, 0.1348963, 0.13803841, 0.14125373,
+                     0.144544, 0.14791083, 0.15135611, 0.1548817,
+                     0.15848931, 0.162181, 0.1659587, 0.1698244,
+                     0.1737801, 0.177828, 0.1819701, 0.18620871,
+                     0.1905461, 0.19498443, 0.1995262, 0.2041738,
+                     0.20892961, 0.2137962, 0.2187762, 0.22387212,
+                     0.2290868, 0.2344229, 0.2398833, 0.2454709,
+                     0.2511886, 0.25703954, 0.2630268, 0.26915344,
+                     0.2754229, 0.2818383, 0.28840312, 0.29512092,
+                     0.3019952, 0.30902954, 0.3162278, 0.32359364,
+                     0.3311311, 0.33884412, 0.34673681, 0.3548134,
+                     0.3630781, 0.37153521, 0.3801894, 0.38904511,
+                     0.39810714, 0.4073803, 0.4168694, 0.42657953,
+                     0.4365158, 0.4466836, 0.4570882, 0.4677351,
+                     0.4786301, 0.48977881, 0.5011872, 0.51286131,
+                     0.5248075, 0.5370318, 0.5495409, 0.5623413,
+                     0.57543992, 0.5888436, 0.6025595, 0.61659491,
+                     0.6309573, 0.6456543, 0.6606934, 0.676083,
+                     0.69183093, 0.7079458, 0.72443592, 0.74131023,
+                     0.7585776, 0.7762471, 0.79432821, 0.81283044,
+                     0.8317637, 0.8511381, 0.8709636, 0.8912509,
+                     0.9120108, 0.93325424, 0.9549925, 0.9772371, 1,
+                     1.023293, 1.047129, 1.0715192, 1.096478,
+                     1.1220182, 1.1481534, 1.1748973, 1.2022641,
+                     1.230269, 1.258926, 1.28825, 1.318257, 1.348963,
+                     1.3803842, 1.4125374, 1.44544, 1.4791082,
+                     1.513561, 1.5488164, 1.584893, 1.62181, 1.659587,
+                     1.698244, 1.737801, 1.7782794, 1.819701,
+                     1.862087, 1.905461, 1.9498444, 1.9952621,
+                     2.041738, 2.089296, 2.137962, 2.187761,
+                     2.2387211, 2.290868, 2.344229, 2.398833,
+                     2.454709, 2.5118863, 2.570396, 2.630268,
+                     2.691535, 2.7542283, 2.818383, 2.884031,
+                     2.951209, 3.019952, 3.090296, 3.162278, 3.235937,
+                     3.311311, 3.3884413, 3.4673681, 3.548134,
+                     3.63078, 3.715352, 3.8018932, 3.890451, 3.981071,
+                     4.073803, 4.168694, 4.2657952, 4.365158,
+                     4.4668354, 4.5708813, 4.677351, 4.7863001,
+                     4.897787, 5.011872, 5.128613, 5.248074,
+                     5.3703184, 5.495409, 5.623413, 5.7543992,
+                     5.8884363, 6.025596, 6.1659493, 6.309573,
+                     6.456542, 6.606934, 6.7608284, 6.9183082,
+                     7.0794563, 7.24436, 7.413103, 7.585776, 7.762471,
+                     7.9432821, 8.1283044, 8.3176364, 8.5113792,
+                     8.709635, 8.912507, 9.120107, 9.332541, 9.549923,
+                     9.7723722, 10, 10.23293, 10.471284, 10.715192,
+                     10.96478, 11.220183, 11.481534, 11.748973,
+                     12.022642, 12.302684, 12.589251, 12.882492,
+                     13.182563, 13.489624, 13.80384, 14.12537,
+                     14.454392, 14.79108, 15.135614, 15.48817,
+                     15.848933, 16.218101, 16.59587, 16.98244,
+                     17.37801, 17.782793, 18.19701, 18.62087,
+                     19.05461, 19.498443, 19.952621, 20.41738,
+                     20.89296, 21.37962, 21.877611, 22.38721,
+                     22.908672, 23.442283, 23.988321, 24.54708,
+                     25.11886, 25.70395, 26.30267, 26.91534,
+                     27.542291, 28.183832, 28.84032, 29.512094,
+                     30.19952, 30.902954, 31.62278, 32.359363,
+                     33.11311, 33.884414, 34.673683, 35.481334,
+                     36.3078, 37.153514, 38.018932, 38.90451,
+                     39.81071, 40.73802, 41.68693, 42.65794, 43.65157,
+                     44.668342, 45.70881, 46.7735, 47.862991,
+                     48.97789, 50.11873, 51.286144, 52.480751,
+                     53.703182, 54.95409, 56.23413, 57.543991,
+                     58.884361, 60.255954, 61.6595, 63.09573,
+                     64.565414, 66.06933, 67.608283, 69.183082,
+                     70.79456, 72.443572, 74.131004, 75.857734,
+                     77.62469, 79.432792, 81.28303, 83.17635,
+                     85.11377, 87.096321, 89.1251, 91.2011, 93.32544,
+                     95.49926, 97.723724, 100])
+    fc = fc[fc<=np.max(freq)]
+    eas_s = smoothed_fas(np.abs(eas), freq, fc, b, w)
+    return([freq, f1, f2, eas, fc, np.sqrt(eas_s)])
