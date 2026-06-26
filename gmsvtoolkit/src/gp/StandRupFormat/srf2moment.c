f01dfd6698ff7f73516d160f2abffcd15d6019be
Fabio Silva <fsilva@usc.edu>
2026-06-26 10:30:48 -0700

    Added srf2moment code

---
+++ gmsvtoolkit/src/gp/StandRupFormat/srf2moment.c
@@ -0,0 +1,35 @@
+#include "include.h"
+#include "structure.h"
+#include "function.h"
+#include "defs.h"
+#include "getpar.h"
+
+int main(int ac,char **av)
+{
+char infile[256], velfile[256];
+
+struct standrupformat srf;
+struct velmodel vmod;
+
+int inbin = 0;
+int use_srf_mu = 0;
+int moment_rate = 0;
+
+sprintf(infile,"stdin");
+sprintf(velfile,"NOT_PROVIDED");
+
+setpar(ac,av);
+getpar("infile","s",infile);
+getpar("velfile","s",velfile);
+getpar("use_srf_mu","d",&use_srf_mu);
+getpar("moment_rate","d",&moment_rate);
+endpar();
+
+read_Fvelmodel(velfile,&vmod);
+read_srf(&srf,infile,inbin);
+
+if(moment_rate)
+   get_moment_rate(&srf,&vmod,use_srf_mu);
+else
+   get_moment(&srf,&vmod,use_srf_mu);
+}
